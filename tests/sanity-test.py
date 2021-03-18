ea827446aba1d49e3bd22daf63dcdff6f3d93441
Circuit-Launch <dan@circuitlaunch.com>
2021-03-17 17:18:50 -0700

    [add] new correct values

--- tests/sanity-test.py
+++ tests/sanity-test.py
@@ -10,7 +10,7 @@ from std_msgs.msg import UInt16
 from sensor_msgs.msg import JointState
 
 head_goal = JointState()
-head_goal.position = [math.radians(90), math.radians(0)]
+head_goal.position = [math.radians(0), math.radians(0)]
 head_goal.name = ["tilt_servo","pan_servo"]
 
 if __name__ == "__main__":
@@ -26,24 +26,27 @@ if __name__ == "__main__":
     rate=rospy.Rate(10)
 
     pub.publish(head_goal)
-    head_goal.position = [math.radians(-90), math.radians(0)]
-    
-    time.sleep(1)
+    time.sleep(5)
+    head_goal.position = [math.radians(0), math.radians(45)]
+    print("1")
 
+    time.sleep(5)
     pub.publish(head_goal)
     head_goal.position = [math.radians(0), math.radians(0)]
-    
-    time.sleep(1)
+    print("2")
+    time.sleep(5)
 
-    pub.publish(head_goal)
-    head_goal.position = [math.radians(90), math.radians(0)]
-    
-    time.sleep(1)
-    pub.publish(head_goal)
+    # pub.publish(head_goal)
+    # head_goal.position = [math.radians(0), math.radians(45)]
+    # print("3")
+    # time.sleep(5)
+    # pub.publish(head_goal)
 
-    head_goal.position = [math.radians(0), math.radians(0)]    
-    time.sleep(1)
-    pub.publish(head_goal)
+    # head_goal.position = [math.radians(0), math.radians(0)]  
+    # print("3")
+  
+    # time.sleep(5)
+    # pub.publish(head_goal)
 
     print("[DONE] Head node...")
 
